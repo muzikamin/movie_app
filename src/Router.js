36b1daa70257e31e806c72e8d9b5291378fffd16
muzikamin <muzikamin@naver.com>
2024-07-12 15:59:22 +0900

    1일차

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,21 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Detail } from "./pages/detail/Detail";
+import { Search } from "./pages/search/Search";
+import { PageNotFound } from "./pages/PageNotFound";
+import { Home } from "./pages/home/Home";
+import { routes } from "./routes";
+
 const Router = () => {
-  return <div>라우터</div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.detail} element={<Detail />} />
+        <Route path={routes.search} element={<Search />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default Router;
