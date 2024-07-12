1a94df64c9c2c0054eb28185c93a538a18c5fa90
muzikamin <muzikamin@naver.com>
2024-07-12 17:37:53 +0900

    2일 차

---
+++ src/api.js
@@ -0,0 +1,19 @@
+const fetch = require("node-fetch");
+
+const baseUrl = "https://api.themoviedb.org/3/";
+// => 3버전
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzNkZTVjM2U5MTcyZjIwYTEwZWFiOGU3MmQ3MjFlMyIsIm5iZiI6MTcyMDc2NTYxMi44OTczOTksInN1YiI6IjY2OGY5NjUwZDhiZThlNGZhZWE3YTAzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kBqyuVYymhjnWruoRae4zPXjAAjg_kVzb0Fsj0v_rY0",
+  },
+};
+
+const url = (urlName) => {
+  return baseUrl + `${urlName}?language=ko-kr`;
+};
+
+export const nowPlaying = () =>
+  fetch(url("movie/now_playing"), options).then((res) => res.json());
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+import { nowPlaying } from "../../api";
+
 export const Home = () => {
+  const [nowData, setNowData] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { results } = await nowPlaying();
+        setNowData(results);
+        // 에러가 발생할 것 같은 것을 넣어주고
+      } catch (error) {
+        console.log(error);
+        // 실제 에러가 났을 때 유저에게 표시할 내용을 넣어주기
+      }
+    })();
+  }, []);
+
+  console.log(nowData);
+
   return <div>Home</div>;
 };
+
+// *예외
+// 1. 컴파일 에러
+// => 프로그램이 실행되기 전에 발생하는 오류
+// => 프로그램 실행 자체가 안 됨
+
+// 2. 런타임 에러
+// => 프로그램이 실행 중 발생하는 오류
+
+// *try ~ catch
+// => 예외가 발생할 것 같은 코드를 제어함
+// ex)
+// try{
+//   => 예외 발생 가능성이 있는 코드 작성
+// }catch(error){
+//   예외가 발생했을 때 처리해주는 부분
+// }finally{
+//   예외와 상관없이 무조건 실행해야하는 코드
+// }
